8d7bdf14fd0d1eab95e3f69ebb4fc6dd09fa6c1c
RainingRePwn <rxx__xxr@163.com>
2024-04-10 05:00:05 -0700

    sure1

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -7,28 +7,40 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod delicious_snacks {
-    // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    // 使用 fruits 模块中的 PEAR 常量
+    //use self::fruits::PEAR;
+    // 使用 veggies 模块中的 CUCUMBER 常量
+    //use self::veggies::CUCUMBER;
 
-    mod fruits {
+    // fruits 模块，包含水果常量定义
+    pub mod fruits {
+        // 定义 PEAR 常量为梨
         pub const PEAR: &'static str = "Pear";
+        // 定义 APPLE 常量为苹果
         pub const APPLE: &'static str = "Apple";
     }
 
-    mod veggies {
+    // veggies 模块，包含蔬菜常量定义
+    pub mod veggies {
+        // 定义 CUCUMBER 常量为黄瓜
         pub const CUCUMBER: &'static str = "Cucumber";
+        // 定义 CARROT 常量为胡萝卜
         pub const CARROT: &'static str = "Carrot";
     }
 }
 
 fn main() {
+    // 将 fruits 和 veggies 模块导入到 main 函数中
+    use delicious_snacks::{fruits, veggies};
+
+    // 打印出 favorite snacks，包括水果和蔬菜
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie
+        // 使用 fruits 模块中的 PEAR 常量
+        fruits::PEAR,
+        // 使用 veggies 模块中的 CUCUMBER 常量
+        veggies::CUCUMBER
     );
 }
+

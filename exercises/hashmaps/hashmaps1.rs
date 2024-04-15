3505e0d4ca60c4473462e317d381a72ab09e54a3
RainingRePwn <rxx__xxr@163.com>
2024-04-15 03:02:39 -0700

    ok

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -11,15 +11,18 @@
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();// TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
+ // 插入更多水果
+    basket.insert(String::from("apple"), 1);
+    basket.insert(String::from("orange"), 1);
+    basket.insert(String::from("grape"), 1);
 
     // TODO: Put more fruits in your basket here.
 
@@ -33,7 +36,7 @@ mod tests {
     #[test]
     fn at_least_three_types_of_fruits() {
         let basket = fruit_basket();
-        assert!(basket.len() >= 3);
+        assert!(basket.len() >= 3); 
     }
 
     #[test]
--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -1,24 +1,6 @@
-// hashmaps2.rs
-//
-// We're collecting different fruits to bake a delicious fruit cake. For this,
-// we have a basket, which we'll represent in the form of a hash map. The key
-// represents the name of each fruit we collect and the value represents how
-// many of that particular fruit we have collected. Three types of fruits -
-// Apple (4), Mango (2) and Lychee (5) are already in the basket hash map. You
-// must add fruit to the basket so that there is at least one of each kind and
-// more than 11 in total - we have a lot of mouths to feed. You are not allowed
-// to insert any more of these fruits!
-//
-// Make me pass the tests!
-//
-// Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
-#[derive(Hash, PartialEq, Eq)]
+#[derive(Hash, PartialEq, Eq, Clone)]
 enum Fruit {
     Apple,
     Banana,
@@ -36,10 +18,21 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Pineapple,
     ];
 
-    for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
-        // basket. Note that you are not allowed to put any type of fruit that's
-        // already present!
+    for fruit in &fruit_kinds {
+        if !basket.contains_key(fruit) {
+            basket.insert(fruit.clone(), 1);
+        }
+    }
+
+    let mut total_fruits: u32 = basket.values().sum();
+
+    while total_fruits <= 11 {
+        for fruit in &fruit_kinds {
+            if !basket.contains_key(fruit) {
+                basket.insert(fruit.clone(), 1);
+                total_fruits += 1;
+            }
+        }
     }
 }
 
@@ -47,7 +40,6 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
 mod tests {
     use super::*;
 
-    // Don't modify this function!
     fn get_fruit_basket() -> HashMap<Fruit, u32> {
         let mut basket = HashMap::<Fruit, u32>::new();
         basket.insert(Fruit::Apple, 4);
